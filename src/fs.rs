8973481b27321845070517d31e6cf4621dca381f
Gene Koval <gene@genya.dev>
2025-07-12 02:01:06 -0400

    fix: change which flags get passed to 'open'

--- src/fs.rs
+++ src/fs.rs
@@ -1,45 +1,53 @@
 use nix::{
-    fcntl::{OFlag, open},
+    fcntl::{self, OFlag},
     sys::stat::Mode,
     unistd::{dup2_stderr, dup2_stdin, dup2_stdout},
 };
 use std::{os::fd::OwnedFd, path::Path};
 
+enum Rw {
+    ReadOnly,
+    WriteOnly,
+}
+
+use Rw::*;
+
 pub fn redirect_stdin() -> Result<(), String> {
     let file = Path::new("/dev/null");
-    let fd = open_file(file)?;
 
-    dup2_stdin(fd).map_err(|err| {
-        format!("failed to redirect stdin to '{}': {err}", file.display())
-    })
+    open(file, ReadOnly)
+        .and_then(|fd| dup2_stdin(fd).map_err(|err| err.to_string()))
+        .map_err(|err| {
+            format!("failed to redirect stdin to '{}': {err}", file.display())
+        })
 }
 
 pub fn redirect_stdout(file: &Path) -> Result<(), String> {
-    let fd = open_file(file)?;
-
-    dup2_stdout(fd).map_err(|err| {
-        format!("failed to redirect stdout to '{}': {err}", file.display())
-    })
+    open(file, WriteOnly)
+        .and_then(|fd| dup2_stdout(fd).map_err(|err| err.to_string()))
+        .map_err(|err| {
+            format!("failed to redirect stdout to '{}': {err}", file.display())
+        })
 }
 
 pub fn redirect_stderr(file: &Path) -> Result<(), String> {
-    let fd = open_file(file)?;
-
-    dup2_stderr(fd).map_err(|err| {
-        format!("failed to redirect stderr to '{}': {err}", file.display())
-    })
+    open(file, WriteOnly)
+        .and_then(|fd| dup2_stderr(fd).map_err(|err| err.to_string()))
+        .map_err(|err| {
+            format!("failed to redirect stderr to '{}': {err}", file.display())
+        })
 }
 
-fn open_file(file: &Path) -> Result<OwnedFd, String> {
-    open(
+fn open(file: &Path, rw: Rw) -> Result<OwnedFd, String> {
+    let flags = match rw {
+        Rw::ReadOnly => OFlag::O_RDONLY,
+        Rw::WriteOnly => OFlag::O_WRONLY | OFlag::O_APPEND,
+    };
+
+    fcntl::open(
         file,
-        OFlag::O_RDWR | OFlag::O_CREAT | OFlag::O_EXCL,
+        flags | OFlag::O_CREAT,
         Mode::S_IRUSR | Mode::S_IWUSR | Mode::S_IRGRP | Mode::S_IWGRP,
     )
-    .map_err(|err| {
-        format!(
-            "failed to open file for redirection '{}': {err}",
-            file.display()
-        )
-    })
+    .map_err(|err| format!("failed to open file: {err}"))
 }
