cc700afed7b2a743fbc7d31d9bfaff177f8ed2bf
Gene Koval <gene@genya.dev>
2025-07-15 02:13:08 -0400

    feat: re-export Mode

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ pub use daemon::{DEFAULT_UMASK, Daemon};
 pub use fork::Parent;
 pub use user::{Group, Privileges, User};
 
+pub use nix::sys::stat::Mode;
+
 pub fn options() -> Daemon {
     Daemon::new()
 }
